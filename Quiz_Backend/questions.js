59dac667de481b04d7de4bbd7785589417982203
Paresh8890-Jaisinghani <136781047+Paresh8890-Jaisinghani@users.noreply.github.com>
2024-05-21 21:24:25 +0530

    First Commit

---
+++ Quiz_Backend/questions.js
@@ -0,0 +1,138 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "These signs mean what? <img src='./image/image1.png' alt='Image'>",
+
+        answer: "Pedestrian crossing.",
+        options: [
+            "Cross-road",
+            "Pedestrian crossing.",
+            "Railway crossing.",
+            "None of these."
+        ]
+    },
+    {
+        numb: 2,
+        question: "These signs mean what? <img src='./image/image2.png' alt='Image'>",
+        answer: "No Parking at any time.",
+        options: [
+            "1 hour parking only.",
+            "5 minute parking allowed.",
+            "No Parking at any time.",
+            "None of these."
+        ]
+    },
+    {
+        numb: 3,
+        question: "These signs mean what? <img src='./image/image3.png' alt='Image'>",
+
+        answer: "Overtaking Prohibited",
+        options: [
+            "Overtaking Prohibited",
+            "No Stopping",
+            "No Parking",
+            "None of these."
+        ]
+    },
+    {
+        numb: 4,
+        question: "These signs mean what? <img src='./image/image4.png' alt='Image'>",
+
+        answer: "Compulsory Turn Left",
+        options: [
+            "Turning left prohibited.",
+            "Compulsory Turn Left",
+            "One way traffic",
+            "None of these."
+        ]
+    },
+    {
+        numb: 5,
+        question: "These signs mean what? <img src='./image/image5.png' alt='Image'>",
+
+        answer: "Two way traffic.",
+        options: [
+            "Two way traffic.",
+            "One lane ahead.",
+            "Proceed with caution.",
+            "None of these."
+        ]
+    },
+    {
+        numb: 6,
+        question: "These signs mean what? <img src='./image/image6.png' alt='Image'>",
+
+        answer: "Start of Dual Carriageway",
+        options: [
+            "Start of Dual Carriageway",
+            "3 lanes of traffic ahead.",
+            "Traffic ends.",
+            "None of these."
+        ]
+    },
+    {
+        numb: 7,
+        question: "These signs mean what? <img src='./image/image7.png' alt='Image'>",
+
+        answer: "Minimum Speed Limit",
+        options: [
+            "Minimum Speed Limit",
+            "Maximum Speed Limit",
+            "Recommended speed ",
+            "None of these."
+        ]
+    },
+    {
+        numb: 8,
+        question: "These signs mean what? <img src='./image/image8.png' alt='Image'>",
+
+        answer: "Police Station Ahead",
+        options: [
+            "Zoo ahead.",
+            "Petrol Pump Ahead",
+            "Police Station Ahead",
+            "None of these."
+        ]
+    },
+    {
+        numb: 9,
+        question: "These signs mean what? <img src='./image/image9.png' alt='Image'>",
+        answer: "Side Road Left",
+        options: [
+            "Y-Intersection",
+            "Side Road Left",
+            "Rest stop ahead.",
+            "None of these."
+        ]
+
+    },
+    {
+        numb: 10,
+        question: "These signs mean what? <img src='./image/image10.png' alt='Image'>",
+
+        answer: "Public Telephone",
+        options: [
+            "Public Telephone",
+            "Parking Area Ahead",
+            "Taxi Stand Ahead",
+            "None of these."
+        ]
+    },
+
+  
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+    // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    //   {
+    //   numb: 6,
+    //   question: "Your Question is Here",
+    //   answer: "Correct answer of the question is here",
+    //   options: [
+    //     "Option 1",
+    //     "option 2",
+    //     "option 3",
+    //     "option 4"
+    //   ]
+    // },
+];
